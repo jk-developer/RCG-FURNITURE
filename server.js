d3ef6d81a2206faf7cb0849992431927ec988bf5
jkgdev <jkgupta4398@gmail.com>
2017-09-02 13:45:11 +0530

    mongoose connect fixed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
 app.use(express.static(__dirname+"/public"));
-mongoose.connect
+
 
 var DBURL = process.env.url;
 if(!DBURL)
